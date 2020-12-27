7b8d6b2ae835e0f4a61119b231105a0326a451f5
BuseMelekOLGAC <76478081+BuseMelekOLGAC@users.noreply.github.com>
2020-12-27 15:30:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+
+def print_hi(name):
+    x = 5
+    print(x)
+    print(f'my favourite number is {x}')
+    print(type(x))
+
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+
+
+
